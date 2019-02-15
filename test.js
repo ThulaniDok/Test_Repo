c5eca1a472957686ac622b817480c093ee8f104d
Thulani <thulanig@bettech.com>
2019-02-15 16:11:16 +0200

    Initial commit :wq

---
+++ test.js
@@ -0,0 +1,14 @@
+// Write a JavaScript program to display the current day and time in the following format
+function timeFormat() {
+    var date = new Date();
+    var day = date.getDay();
+    var days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    
+    console.log("Today is : " + days[day]);
+    console.log("Current time is : " + hours + " PM : " + minutes + " : " +  seconds);
+}
+
+timeFormat();
